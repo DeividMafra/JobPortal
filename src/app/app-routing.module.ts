533325decf0da43a11848a357f9a092e128e5795
Deivid Mafra <deividmafra@aol.com>
2020-06-29 18:44:14 -0400

    NavBar Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JobListComponent } from './job-seeker/job-list/job-list.component';
+import { JobDetailsComponent } from './job-seeker/job-details/job-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: JobListComponent
+  },
+  {
+    path: ':jobId/details',
+    component: JobDetailsComponent
   },
 ];
 
