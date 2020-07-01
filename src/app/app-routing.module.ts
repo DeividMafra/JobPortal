cad06f18864a5206e130234ee834401e76d9c0e1
Deivid Mafra <deividmafra@aol.com>
2020-07-01 15:59:23 -0400

    Job List component done; Adjusted some stuff on navbar and footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JobListComponent } from './job-seeker/job-list/job-list.component';
 import { JobDetailsComponent } from './job-seeker/job-details/job-details.component';
+import { PostJobComponent } from './recruiter/post-job/post-job.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: ':jobId/details',
     component: JobDetailsComponent
+  },
+  {
+    path: 'post-job',
+    component: PostJobComponent
   },
 ];
 
--- src/app/job-seeker/job-list/job-list.component.ts
+++ src/app/job-seeker/job-list/job-list.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface JobPost {
+  id: number;
+  title: string;
+  type: Array<string>;
+  address: string;
+  postDays: number;
+  logoPath: string;
+}
 @Component({
   selector: 'app-job-list',
   templateUrl: './job-list.component.html',
@@ -7,9 +15,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobListComponent implements OnInit {
 
+  public posts: JobPost[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.fakeData();
+  }
+
+  fakeData = () => {
+    this.posts = [
+      {
+        id: 1,
+        title: "Web Designer, Graphic Designer, UI/UX Designer",
+        type: ["Full time", "Contract"],
+        address: "Wellesley Road, London",
+        postDays: 9,
+        logoPath: "../../../assets/logo1.jpg"
+      },
+      {
+        id: 2,
+        title: "Web Developer & Software Developer",
+        type: ["Full time"],
+        address: "Garden Road, UK",
+        postDays: 5,
+        logoPath: "../../../assets/logo2.jpg"
+      },
+      {
+        id: 3,
+        title: "Application Developer & Web Designer",
+        type: ["Part time"],
+        address: "Drive Post NY 676",
+        postDays: 1,
+        logoPath: "../../../assets/logo3.jpg"
+      },
+      {
+        id: 4,
+        title: "IT Department & Manager",
+        type: ["Full time", "Contract", "Freelancer"],
+        address: "Drive Post NY 676",
+        postDays: 1,
+        logoPath: "../../../assets/logo4.jpg"
+      },
+      {
+        id: 5,
+        title: "Office Assistant",
+        type: ["Intern"],
+        address: "Wellesley Road, London",
+        postDays: 10,
+        logoPath: "../../../assets/logo5.jpg"
+      },
+      {
+        id: 6,
+        title: "Web Designer, Graphic Designer, UI/UX Designer",
+        type: ["Full time", "Contract"],
+        address: "Wellesley Road, London",
+        postDays: 9,
+        logoPath: "../../../assets/logo1.jpg"
+      },
+      {
+        id: 7,
+        title: "Web Developer & Software Developer",
+        type: ["Full time"],
+        address: "Garden Road, UK",
+        postDays: 5,
+        logoPath: "../../../assets/logo2.jpg"
+      },
+      {
+        id: 8,
+        title: "Application Developer & Web Designer",
+        type: ["Part time"],
+        address: "Drive Post NY 676",
+        postDays: 1,
+        logoPath: "../../../assets/logo3.jpg"
+      },
+      {
+        id: 9,
+        title: "IT Department & Manager",
+        type: ["Full time", "Contract", "Freelancer"],
+        address: "Drive Post NY 676",
+        postDays: 1,
+        logoPath: "../../../assets/logo4.jpg"
+      },
+      {
+        id: 10,
+        title: "Office Assistant",
+        type: ["Intern"],
+        address: "Wellesley Road, London",
+        postDays: 10,
+        logoPath: "../../../assets/logo5.jpg"
+      },
+    ]
+
+    console.log('this.posts', this.posts)
   }
 
 }
