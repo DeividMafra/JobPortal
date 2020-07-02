cdeff2aa56ae861aa1ef620cf3e8cc98352409be
Deivid Mafra <deividmafra@aol.com>
2020-07-02 17:52:10 -0400

    Background for search engine done.

--- src/app/job-seeker/job-list/search-job/search-job.component.ts
+++ src/app/job-seeker/job-list/search-job/search-job.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+interface Category {
+  id: number,
+  description: string
+}
 
 @Component({
   selector: 'app-search-job',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchJobComponent implements OnInit {
 
+  public categories: Category[];
+
+  searchForm = new FormGroup({
+    jobTitle: new FormControl(''),
+    location: new FormControl(''),
+    category: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    this.categories = [
+      {
+        id: 1,
+        description: "Web Development",
+      },
+      {
+        id: 2,
+        description: "Graphics Design",
+      },
+      {
+        id: 3,
+        description: "Data Entry",
+      },
+      {
+        id: 4,
+        description: "Visual Editor",
+      },
+      {
+        id: 5,
+        description: "Office Assistant",
+      },
+      {
+        id: 6,
+        description: "IT Manager",
+      },
+    ]
+
+    console.log('this.categories ', this.categories)
   }
 
 }
